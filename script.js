802a2b7ba2cee6390ea8a041ed4ce8e77513e228
aevanforeest <aevanforeest@users.noreply.github.com>
2023-04-15 07:27:59 +0200

    Add service worker

--- script.js
+++ script.js
@@ -124,4 +124,13 @@ function saveNewTeam() {
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err));
+    });
+  }
+
 });
