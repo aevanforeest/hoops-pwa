7a556b43f1af0b364d5e54adc2c1c22a62caee43
aevanforeest <aevanforeest@users.noreply.github.com>
2023-04-16 21:33:57 +0200

    id

--- teams.js
+++ teams.js
@@ -1,19 +1,16 @@
-function renderPage(db, key) {
+function renderPage(db) {
   const transaction = db.transaction('teams', 'readonly');
   const teamsStore = transaction.objectStore('teams');
-  teamsStore.getAllKeys().onsuccess = function(event) {
-    const teamKeys = event.target.result;
+  teamsStore.getAll().onsuccess = function(event) {
+    const teams = event.target.result;
     const list = document.querySelector('#teamList');
     list.innerHTML = '';
-    teamKeys.forEach(function(teamKey) {
-      teamsStore.get(teamKey).onsuccess = function(event) {
-        const team = event.target.result;
-        const item = document.createElement('li');
-        item.innerHTML =
-          // '<img src="data:image/png;base64,' + team.logo + '" />' +
-          '<a href="team.html?key=' + teamKey + '">' + team.name + '</a>';
-        list.append(item);
-      }
+    teams.forEach(function(team) {
+      const item = document.createElement('li');
+      item.innerHTML =
+        // '<img src="data:image/png;base64,' + team.logo + '" />' +
+        '<a href="team.html?id=' + team.id + '">' + team.name + '</a>';
+      list.append(item);
     });
   };
 }
