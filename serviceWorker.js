f970184fbee6ea53b905e2a38926312ee1739f40
aevanforeest <aevanforeest@users.noreply.github.com>
2023-04-20 16:59:20 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,41 +4,40 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
     .then(cache => cache.addAll([
-      '/',
-      '/index.html',
-      '/justkeepstats.png',
-      '/style.css',
-      '/script.js',
-      '/teams.html',
-      '/teams.js',
-      '/team.html',
-      '/team.js',
-      '/newTeam.html',
-      '/newTeam.js',
-      '/editTeam.html',
-      '/editTeam.js',
-      '/player.html',
-      '/player.js',
-      '/newPlayer.html',
-      '/newPlayer.js',
-      '/editPlayer.html',
-      '/editPlayer.js',
-      '/game.html',
-      '/game.js',
-      '/newGame.html',
-      '/newGame.js',
-      '/editGame.html',
-      '/editGame.js'
+      'index.html',
+      'justkeepstats.png',
+      'style.css',
+      'script.js',
+      'teams.html',
+      'teams.js',
+      'team.html',
+      'team.js',
+      'newTeam.html',
+      'newTeam.js',
+      'editTeam.html',
+      'editTeam.js',
+      'player.html',
+      'player.js',
+      'newPlayer.html',
+      'newPlayer.js',
+      'editPlayer.html',
+      'editPlayer.js',
+      'game.html',
+      'game.js',
+      'newGame.html',
+      'newGame.js',
+      'editGame.html',
+      'editGame.js'
     ]))
   );
 });
 
 self.addEventListener('fetch', event => {
-  console.log(event.request.url);
   event.respondWith(
     caches.match(event.request, { ignoreSearch: true })
     .then(response => {
       if (response) {
+        console.log('cached', event.request);
         return response;
       }
       return fetch(event.request);
